9b0954ab430f7ef701f606b10fa77c82a02b43cb
dinglimingzh <dinglimingzh@foxmail.com>
2018-12-17 19:30:42 +0800

    modify configs

--- release.sh
+++ release.sh
@@ -1,8 +1,3 @@
-wget -c --user=reader --password=123456 http://hk.nexus.op.easemob.com/nexus/service/local/repositories/releases/content/com/easemob/ai/robotapi/robotapi-ui/1.20.3.FINAL/robotapi-ui-1.20.3.FINAL.jar
-wget -c --user=reader --password=123456 http://hk.nexus.op.easemob.com/nexus/service/local/repositories/releases/content/com/easemob/ai/robotapi/kefu-robotapi/1.20.4.FINAL/kefu-robotapi-1.20.4.FINAL.jar
 wget -c --user=reader --password=123456 http://hk.nexus.op.easemob.com/nexus/service/local/repositories/releases/content/com/easemob/ai/robotapi/nlu-server/1.7.4.FINAL/nlu-server-1.7.4.FINAL.jar
-wget -c --user=reader --password=123456 http://hk.nexus.op.easemob.com/nexus/service/local/repositories/releases/content/com/easemob/ai/robotapi/domain-intention-server/1.12.18.FINAL/domain-intention-server-1.12.18.FINAL.jar
-wget -c --user=reader --password=123456 http://hk.nexus.op.easemob.com/nexus/service/local/repositories/releases/content/com/easemob/ai/robotapi/intention-classify-server/1.12.18.FINAL/intention-classify-server-1.12.18.FINAL.jar
-wget -c --user=reader --password=123456 http://hk.nexus.op.easemob.com/nexus/service/local/repositories/releases/content/com/easemob/ai/robotapi/common-intention-server/1.12.18.FINAL/common-intention-server-1.12.18.FINAL.jar
 wget -c --user=reader --password=123456 http://hk.nexus.op.easemob.com/nexus/service/local/repositories/releases/content/com/easemob/ai/robotapi/faq-server/1.20.6.FINAL/faq-server-1.20.6.FINAL.jar
 wget -c --user=reader --password=123456 http://hk.nexus.op.easemob.com/nexus/service/local/repositories/releases/content/com/easemob/robot/robot-training-server/47.2.30.FINAL/robot-training-server-47.2.30.FINAL.jar
