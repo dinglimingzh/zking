c720ef47c4f7e8c131e676c2f4bd1e9f85ea3216
dinglimingzh <dinglimingzh@foxmail.com>
2018-12-19 15:25:58 +0800

    commit

--- 1.sh
+++ 1.sh
@@ -1,1 +1,1 @@
-wget -t 0 -c http://hk.nexus.op.easemob.com/nexus/service/local/repositories/releases/content/com/easemob/ai/robotapi/nlu-server/1.7.14.FINAL/nlu-server-1.7.14.FINAL.jar
+wget -t 0 -c --user=reader --password=123456 http://hk.nexus.op.easemob.com/nexus/service/local/repositories/releases/content/com/easemob/ai/robotapi/nlu-server/1.7.14.FINAL/nlu-server-1.7.14.FINAL.jar
