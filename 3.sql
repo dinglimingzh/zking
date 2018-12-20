b6cd468afe7f63538de358abaa507ced143b77b6
dinglimingzh <dinglimingzh@foxmail.com>
2018-12-20 11:43:35 +0800

    modify configs

--- 3.sql
+++ 3.sql
@@ -1,2 +1,2 @@
 use webapp;
-insert into options(optionId,tenantId,optionName,optionValue,createDateTime,lastUpdateDateTime) value (uuid(),43,'saasLoginUrl',' https://robot.zking.com',NOW(),NOW());
+insert into options(optionId,tenantId,optionName,optionValue,createDateTime,lastUpdateDateTime) value(uuid(),43,'saasLoginUrl',' https://robot.zking.com',NOW(),NOW());
