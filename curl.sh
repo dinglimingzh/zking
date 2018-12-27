0e7cc0e5a046423d95daa8367a80e42c70057134
dinglimingzh <dinglimingzh@foxmail.com>
2018-12-27 17:45:54 +0800

    modify configs

--- curl.sh
+++ curl.sh
@@ -1,5 +1,2 @@
 curl -X POST "https://kefurobot.zking.com/v1/weixin/open/wx001/callback?msg_signature=222"
-
-
-
 curl -X POST "http://kefurobot.zking.com/v1/weixin/open/wx001/callback?msg_signature=222"
