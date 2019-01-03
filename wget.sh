b700d33112580dba290e7254b83dc8972de74e39
dinglimingzh <dinglimingzh@foxmail.com>
2019-01-03 12:40:52 +0800

    modify configs

---
+++ wget.sh
@@ -0,0 +1,1 @@
+wget --user=reader --password=123456 http://hk.nexus.op.easemob.com/nexus/service/local/repositories/releases/content/com/easemob/kefu/kefu-robot-integration-server/47.4.15.FINAL/kefu-robot-integration-server-47.4.15.FINAL.jar
