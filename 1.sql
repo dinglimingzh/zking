5862ea732f5acb2f44eac36386597b62e46e246b
dinglimingzh <dinglimingzh@foxmail.com>
2019-01-04 16:52:10 +0800

    modify configs

---
+++ 1.sql
@@ -0,0 +1,4 @@
+use robot_integration;
+select * from robot where robotAgentId='abd3b006-0451-45ec-a33b-161bb0feeee5' limit 1;
+use webapp;
+select * from agentuser where userId='abd3b006-0451-45ec-a33b-161bb0feeee5' limit 1;
