c790692a5bef9348ae5385780ba65a4878f96184
dingliming <dinglimingzh@foxmail.com>
2019-01-12 18:32:14 +0800

    modify configs

--- wget.sh
+++ wget.sh
@@ -1,5 +1,1 @@
-wget -c -t 0 --user=reader --password=123456 http://hk.nexus.op.easemob.com/nexus/service/local/repositories/releases/content/com/easemob/ai/robotapi/dm-config-server/1.7.11.FINAL/dm-config-server-1.7.11.FINAL.jar
-wget -c -t 0 --user=reader --password=123456 http://hk.nexus.op.easemob.com/nexus/service/local/repositories/releases/content/com/easemob/ai/robotapi/nlu-server/1.7.16.FINAL/nlu-server-1.7.16.FINAL.jar
-wget -c -t 0 --user=reader --password=123456 http://hk.nexus.op.easemob.com/nexus/service/local/repositories/releases/content/com/easemob/ai/robotapi/kefu-robotapi/1.20.23.FINAL/kefu-robotapi-1.20.23.FINAL.jar
-wget -c -t 0 --user=reader --password=123456 http://hk.nexus.op.easemob.com/nexus/service/local/repositories/releases/content/com/easemob/robot/rest-server/47.9.10.ZKING.FINAL/rest-server-47.9.10.ZKING.FINAL.jar
-
+wget -c -t 0 --user=reader --password=123456 http://hk.nexus.op.easemob.com/nexus/service/local/repositories/releases/content/com/easemob/ai/robotapi/kg-server/1.7.8.FINAL/kg-server-1.7.8.FINAL.jar
